b8eea1133b0e2f8446424fdde42de016b58c70d4
Christian Gutierrez <christiandg1308@gmail.com>
2025-06-10 20:49:12 -0400

    Topologias y Cambios

--- src/main/java/org/sakidoa/Main.java
+++ src/main/java/org/sakidoa/Main.java
@@ -1,15 +1,58 @@
 package org.sakidoa;
 
 
-public class Main {
-    public static void main(String[] args) {
-		System.out.println("Welcome to the Sakidoa application!");
+import org.sakidoa.core.*;
+import org.sakidoa.*;
+import org.sakidoa.fullyconnectednetwork.FullyConnectedNetwork;
+import org.sakidoa.meshnetwork.MeshNetwork;
+import org.sakidoa.starnetwork.StarNetwork;
+import org.sakidoa.switchednetwork.SwitchedNetwork;
 
-		// Example of how to use the NetworkTopology interface
-		// NetworkTopology network = new NetworkTopologyImpl();
-		// network.configureNetwork(5);
-		// network.sendMessage(1, 2, "Hello from Node 1 to Node 2");
-		// network.runNetwork();
-		// network.shutdownNetwork();
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws InterruptedException {
+		Scanner scanner = new Scanner(System.in);
+		NetworkTopology topology = null;
+
+		System.out.println("=== SIMULADOR DE TOPOLOGÍAS DE RED ===");
+		System.out.println("1. Mesh Network");
+		System.out.println("2. Star Network");
+		System.out.println("3. Fully Connected Network");
+		System.out.println("4. Switched Network");
+		System.out.print("Selecciona una opción: ");
+		int option = scanner.nextInt();
+
+		System.out.print("Cantidad de nodos: ");
+		int numNodes = scanner.nextInt();
+
+		switch (option) {
+			case 1 -> topology = new MeshNetwork();
+			case 2 -> topology = new StarNetwork();
+			case 3 -> topology = new FullyConnectedNetwork();
+			case 4 -> topology = new SwitchedNetwork();
+			default -> {
+				System.out.println("Opción inválida.");
+				System.exit(0);
+			}
+		}
+
+		topology.configureNetwork(numNodes);
+		topology.runNetwork();
+
+		System.out.println("\nRed iniciada. Enviando mensajes de prueba...");
+		
+		for (int i = 0; i < numNodes; i++) {
+			int to = (i + 1) % numNodes;
+			topology.sendMessage(i, to, "Hola de nodo " + i + " a nodo " + to);
+			Thread.sleep(500);
+		}
+
+		System.out.println("\nPresiona ENTER para detener la red...");
+		scanner.nextLine();
+		scanner.nextLine();
+
+		topology.shutdownNetwork();
+		System.out.println("Red detenida.");
 	}
 }
