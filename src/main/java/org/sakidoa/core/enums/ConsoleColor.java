93e38dc388c25367cdf072ef7a84e73f11d4cea7
Cristian de la Hoz <cyrondelahoz@gmail.com>
2025-06-12 22:44:43 -0400

    feat: add ConsoleColor enum for terminal color codes

---
+++ src/main/java/org/sakidoa/core/enums/ConsoleColor.java
@@ -0,0 +1,50 @@
+package org.sakidoa.core.enums;
+
+public enum ConsoleColor {
+    RESET("\u001B[0m"),
+    
+    // Text colors
+    BLACK("\u001B[30m"),
+    RED("\u001B[31m"),
+    GREEN("\u001B[32m"),
+    YELLOW("\u001B[33m"),
+    BLUE("\u001B[34m"),
+    PURPLE("\u001B[35m"),
+    CYAN("\u001B[36m"),
+    WHITE("\u001B[37m"),
+    
+    // Bright colors
+    BRIGHT_BLACK("\u001B[90m"),
+    BRIGHT_RED("\u001B[91m"),
+    BRIGHT_GREEN("\u001B[92m"),
+    BRIGHT_YELLOW("\u001B[93m"),
+    BRIGHT_BLUE("\u001B[94m"),
+    BRIGHT_PURPLE("\u001B[95m"),
+    BRIGHT_CYAN("\u001B[96m"),
+    BRIGHT_WHITE("\u001B[97m"),
+    
+    // Background colors
+    BG_BLACK("\u001B[40m"),
+    BG_RED("\u001B[41m"),
+    BG_GREEN("\u001B[42m"),
+    BG_YELLOW("\u001B[43m"),
+    BG_BLUE("\u001B[44m"),
+    BG_PURPLE("\u001B[45m"),
+    BG_CYAN("\u001B[46m"),
+    BG_WHITE("\u001B[47m");
+
+    private final String code;
+
+    ConsoleColor(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String toString() {
+        return code;
+    }
+}
